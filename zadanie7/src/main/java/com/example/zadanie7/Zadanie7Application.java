5c36f545944b56a0d5aa0219aff078a8302bbdf3
JustCode12 <monica.czaplinska@gmail.com>
2019-04-13 00:04:32 +0200

    rozwiązanie zadania newsletter #7

---
+++ zadanie7/src/main/java/com/example/zadanie7/Zadanie7Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie7Application.class, args);
+    }
+
+}
---
+++ zadanie7/src/main/java/com/example/zadanie7/controller/CounterController.java
@@ -0,0 +1,35 @@
+package com.example.zadanie7.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+@Controller
+public class CounterController {
+
+    private int globalCounter;
+    private HashMap<String, Integer> sessions = new HashMap<>();
+
+    @RequestMapping("/")
+    public String home(HttpSession httpSession, Model model) {
+
+        //increases global counter
+        globalCounter++;
+        model.addAttribute("globalCounter", globalCounter);
+        //
+        String currentSession = httpSession.getId();
+        if (!sessions.containsKey(currentSession)) {
+            sessions.put(currentSession, 1);
+        } else {
+            int count = sessions.get(currentSession);
+            sessions.replace(currentSession, count + 1);
+        }
+        int sessionCounter = sessions.get(currentSession);
+        model.addAttribute("sessionCounter", sessionCounter);
+        System.out.println(currentSession);
+        return "index";
+    }
+}
